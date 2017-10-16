9744c4a2b292b79f1a4d6e40e65439120215bd49
Brynjarb16 <brynjarbarkarson@Brynjars-MacBook-Pro.local>
2017-10-16 14:58:16 +0000

    two numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,15 +1,14 @@
 package is.ru.stringcalculator;
 
-public class Calculator{
-	
-	public static int add(String text){
-		if(text.equals("")){
+public class Calculator {
+	public static int add (String text) {
+		if (text.equals(""))
 			return 0;
-		} else {
-		if(text.contains(",")){
+		else 
+			if (text.contains(",")) {
+				String [] numbers = text.split(",");
+				return (Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]));
+			}
 			return 1;
-		}
 	}
-	return 1;
-	}	
 }
